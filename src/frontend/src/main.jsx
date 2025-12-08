f28e81ec082b19c472665255bcc596a42c63bd63
nidhichaphale <nidhichaphale@gmail.com>
2025-12-08 18:47:28 +0530

    Refactor main.jsx to simplify rendering structure

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Context from './Context.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './q.css'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <Context>
-      <App />
-    </Context>
-    
-    </BrowserRouter>
-    
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
